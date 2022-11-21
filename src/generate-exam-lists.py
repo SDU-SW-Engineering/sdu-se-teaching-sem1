94110afb36ff0a73ae25c1952ed0aa721d94ffe6
Aslak Johansen <aslakj@gmail.com>
2022-11-21 09:54:26 +0100

    Added script from parsing lists of students, copy-pasted from itslearning

---
+++ src/generate-exam-lists.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+def groups2thold (groups):
+  for group in groups:
+    if len(group)==2 and group[0]=="T":
+      return group
+  return -1
+
+def groups2group (groups):
+  for group in groups:
+    if group.startswith("Gruppe "):
+      return group
+  return -1
+
+def load_datafile (filename, students=None):
+  if students==None: students = []
+  
+  with open(filename) as fo:
+    lines = fo.readlines()
+  
+  for line in lines:
+    elements = line.split("\t")
+    name   = elements[1]
+    email  = elements[2]
+    role   = elements[3].strip()
+    groups = elements[4].split(", ")
+    thold  = groups2thold(groups)
+    group  = groups2group(groups)
+    
+    if not role in ["Studerende", "Student"]: continue
+    
+    student = {
+      "name": name,
+      "email": email,
+      "role": role,
+      "thold": thold,
+      "group": group,
+    }
+    students.append(student)
+  
+  return students
+
+oop_students = []
+load_datafile("oop1.data", oop_students)
+load_datafile("oop2.data", oop_students)
+
+sem_students = []
+load_datafile("sem1.data", sem_students)
+load_datafile("sem2.data", sem_students)
+
+#print(oop_students)
+#print(sem_students)
+
