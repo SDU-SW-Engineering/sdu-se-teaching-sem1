5340d3c334d7078c5248d0bdd71baebb03e66336
Aslak Johansen <aslakj@gmail.com>
2022-11-25 14:07:27 +0100

    Loaded student to education line mapping

--- src/generate-exam-lists.py
+++ src/generate-exam-lists.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+from openpyxl import load_workbook
+
+src_sheet_names = [
+  "Software Engineering",
+  "Software teknologi",
+  "Spiludvikling og Læringsteknolo",
+]
+
+eduname2line = {
+  "Software Engineering":            "Software Engineering",
+  "Software teknologi":              "Softwareteknologi",
+  "Spiludvikling og Læringsteknolo": "Spiludvikling og Læringsteknologi",
+}
+
 def groups2thold (groups):
   for group in groups:
     if len(group)==2 and group[0]=="T":
@@ -40,6 +54,26 @@ def load_datafile (filename, students=None):
   
   return students
 
+def load_student_lines (filename):
+  global name2line
+  
+  wb = load_workbook(filename=filename)
+  
+  for sheet_name in src_sheet_names:
+    line = eduname2line[sheet_name]
+    sheet = wb[sheet_name]
+    
+    for row in range(2, 200):
+      if sheet["A%d"%row].value==None:
+        
+        break
+      
+      gname = sheet["B%d"%row].value
+      fname = sheet["C%d"%row].value
+      name = "%s %s" % (gname, fname)
+      
+      name2line[name] = line
+
 oop_students = []
 load_datafile("oop1.data", oop_students)
 load_datafile("oop2.data", oop_students)
@@ -48,6 +82,10 @@ sem_students = []
 load_datafile("sem1.data", sem_students)
 load_datafile("sem2.data", sem_students)
 
+name2line = {}
+load_student_lines("Lister SI1-OOP19 med klasser.xlsx")
+
 #print(oop_students)
 #print(sem_students)
+#print(name2line)
 
